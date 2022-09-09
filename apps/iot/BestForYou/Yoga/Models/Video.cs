763cdbdf2e5e1aa1597ed506a2a7d83c649c754f
ladc <72278371+WuFdev@users.noreply.github.com>
2022-09-09 17:40:53 -0400

    Delete apps/iot directory

--- apps/iot/BestForYou/Yoga/Models/Video.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Yoga.Models
-{
-    public class Video
-    {
-        public TrainingVideo[] Suggested { get; set; }
-        public TrainingVideo[] Completed { get; set; }
-        public TrainingVideo[] Browse { get; set; }
-    }
-}
--- apps/iot/BestForYou/Yoga/Models/YogaData.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Yoga.Models
-{
-    public class YogaData
-    {
-        public Pose[] Poses { get; set; }
-
-        public Video[] Videos { get; set; }
-    }
-}
