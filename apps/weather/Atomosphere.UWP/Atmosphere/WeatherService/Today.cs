8495bb4678e54e99a415957e7eb70326ca640319
ladc <72278371+WuFdev@users.noreply.github.com>
2022-09-09 17:39:44 -0400

    Delete apps/weather directory

--- apps/weather/Atomosphere.UWP/Atmosphere/WeatherService/Today.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Atmosphere
-{
-    public sealed class Today 
-    {
-        public string TimeOfDay { get; set; }
-        public int Temperature { get; set; }
-        public string Weather { get; set; }
-    }
-}
--- apps/weather/Atomosphere.UWP/Atmosphere/WeatherService/WeatherData.cs
+++
@@ -1,9 +0,0 @@
-namespace Atmosphere
-{
-    public sealed class WeatherData 
-    {
-        public string[] Locations { get; set; }
-        public Today[] Today { get; set; }
-        public WeekDay[] WeekDay { get; set; }
-    }
-}
